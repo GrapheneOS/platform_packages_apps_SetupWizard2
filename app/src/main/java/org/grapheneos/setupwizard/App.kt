1f868b0a78c8345e16671910caf27fb1a2521ce3
Jitesh Singh <jiteshsingh.jts@gmail.com>
2023-12-27 14:54:21 +0530

    material 3 with dynamic colors

--- app/src/main/java/org/grapheneos/setupwizard/App.kt
+++ app/src/main/java/org/grapheneos/setupwizard/App.kt
@@ -1,10 +1,12 @@
 package org.grapheneos.setupwizard
 
 import android.app.Application
+import com.google.android.material.color.DynamicColors
 
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
         appContext = this
+        DynamicColors.applyToActivitiesIfAvailable(this)
     }
 }
