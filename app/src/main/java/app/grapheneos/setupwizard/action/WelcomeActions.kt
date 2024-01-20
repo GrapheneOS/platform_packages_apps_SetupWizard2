6644264641fe122a2cc3abe120c62131f6c6facc
Jitesh Singh <jiteshsingh.jts@gmail.com>
2024-01-21 00:24:40 +0530

    displays sim locale as first entry in language chooser

--- app/src/main/java/app/grapheneos/setupwizard/action/WelcomeActions.kt
+++ app/src/main/java/app/grapheneos/setupwizard/action/WelcomeActions.kt
@@ -3,11 +3,15 @@ package app.grapheneos.setupwizard.action
 import android.app.Activity
 import android.app.AlertDialog
 import android.content.Intent
+import android.telephony.TelephonyManager
 import android.util.Log
-import com.android.internal.app.LocalePicker
-import com.google.android.setupcompat.util.SystemBarHelper
+import android.widget.ArrayAdapter
 import app.grapheneos.setupwizard.R
+import app.grapheneos.setupwizard.appContext
 import app.grapheneos.setupwizard.data.WelcomeData
+import com.android.internal.app.LocalePicker
+import com.android.internal.app.LocalePicker.LocaleInfo
+import com.google.android.setupcompat.util.SystemBarHelper
 
 object WelcomeActions {
     private const val TAG = "WelcomeActions"
@@ -25,7 +29,7 @@ object WelcomeActions {
     }
 
     fun showLanguagePicker(activity: Activity) {
-        val adapter = LocalePicker.constructAdapter(activity)
+        val adapter = constructLocaleAdapter(activity)
         AlertDialog.Builder(activity)
             .setTitle(R.string.choose_your_language)
             .setAdapter(adapter) { _, which ->
@@ -37,7 +41,7 @@ object WelcomeActions {
             .create().show()
     }
 
-    private fun updateLocale(locale: LocalePicker.LocaleInfo) {
+    private fun updateLocale(locale: LocaleInfo) {
         LocalePicker.updateLocale(locale.locale)
         refreshCurrentLocale()
     }
@@ -52,5 +56,22 @@ object WelcomeActions {
 
     fun emergencyCall(context: Activity) {
         SetupWizard.startActivity(context, Intent(ACTION_EMERGENCY))
+    }
+
+    private fun constructLocaleAdapter(activity: Activity): ArrayAdapter<LocaleInfo> {
+        val adapter = LocalePicker.constructAdapter(activity)
+        val simLocale =
+            appContext.getSystemService(TelephonyManager::class.java).simLocale ?: return adapter
+        var localeInfo: LocaleInfo? = null
+        for (index in 0..adapter.count) {
+            val item = adapter.getItem(index)
+            if (!item?.locale?.toLanguageTag().equals(simLocale.toLanguageTag())) continue
+            Log.d(TAG, "constructLocaleAdapter: found simLocale $simLocale")
+            localeInfo = item
+            adapter.remove(localeInfo)
+            break
+        }
+        if (localeInfo != null) adapter.insert(localeInfo, 0)
+        return adapter
     }
 }
