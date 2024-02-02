15c2ad072f93da1b22418d4726c93974f9815036
Jitesh Singh <jiteshsingh.jts@gmail.com>
2024-02-03 02:14:42 +0530

    applies sim language on setup wizard entry
    also introduces a flag to toggle this behaviour

--- app/src/main/java/app/grapheneos/setupwizard/Constants.kt
+++ app/src/main/java/app/grapheneos/setupwizard/Constants.kt
@@ -2,6 +2,7 @@ package app.grapheneos.setupwizard
 
 import android.content.Context
 
+const val APPLY_SIM_LANGUAGE_ON_ENTRY = true
 const val USE_LEGACY_WIFI_SETUP = false
 
 lateinit var appContext: Context
--- app/src/main/java/app/grapheneos/setupwizard/action/WelcomeActions.kt
+++ app/src/main/java/app/grapheneos/setupwizard/action/WelcomeActions.kt
@@ -6,17 +6,20 @@ import android.content.Intent
 import android.telephony.TelephonyManager
 import android.util.Log
 import android.widget.ArrayAdapter
+import app.grapheneos.setupwizard.APPLY_SIM_LANGUAGE_ON_ENTRY
 import app.grapheneos.setupwizard.R
 import app.grapheneos.setupwizard.appContext
 import app.grapheneos.setupwizard.data.WelcomeData
 import com.android.internal.app.LocalePicker
 import com.android.internal.app.LocalePicker.LocaleInfo
 import com.google.android.setupcompat.util.SystemBarHelper
+import java.util.Locale
 
 object WelcomeActions {
     private const val TAG = "WelcomeActions"
     private const val ACTION_ACCESSIBILITY = "android.settings.ACCESSIBILITY_SETTINGS_FOR_SUW"
     private const val ACTION_EMERGENCY = "com.android.phone.EmergencyDialer.DIAL"
+    private var simLocaleApplied = false
 
     init {
         refreshCurrentLocale()
@@ -26,6 +29,7 @@ object WelcomeActions {
     fun handleEntry(context: Activity) {
         SetupWizard.setStatusBarHidden(true)
         SystemBarHelper.setBackButtonVisible(context.window, false)
+        if (APPLY_SIM_LANGUAGE_ON_ENTRY) applySimLocale()
     }
 
     fun showLanguagePicker(activity: Activity) {
@@ -34,15 +38,22 @@ object WelcomeActions {
             .setTitle(R.string.choose_your_language)
             .setAdapter(adapter) { _, which ->
                 updateLocale(
-                    adapter.getItem(which)
+                    adapter.getItem(which).locale
                 )
             }
             .setNegativeButton(R.string.cancel) { dialog, _ -> dialog.dismiss() }
             .create().show()
     }
 
-    private fun updateLocale(locale: LocaleInfo) {
-        LocalePicker.updateLocale(locale.locale)
+    private fun applySimLocale() {
+        if (simLocaleApplied) return
+        val simLocale = getSimLocale() ?: return
+        updateLocale(simLocale)
+        simLocaleApplied = true
+    }
+
+    private fun updateLocale(locale: Locale) {
+        LocalePicker.updateLocale(locale)
         refreshCurrentLocale()
     }
 
@@ -58,10 +69,13 @@ object WelcomeActions {
         SetupWizard.startActivity(context, Intent(ACTION_EMERGENCY))
     }
 
+    private fun getSimLocale(): Locale? {
+        return appContext.getSystemService(TelephonyManager::class.java).simLocale
+    }
+
     private fun constructLocaleAdapter(activity: Activity): ArrayAdapter<LocaleInfo> {
         val adapter = LocalePicker.constructAdapter(activity)
-        val simLocale =
-            appContext.getSystemService(TelephonyManager::class.java).simLocale ?: return adapter
+        val simLocale = getSimLocale() ?: return adapter
         var localeInfo: LocaleInfo? = null
         for (index in 0..adapter.count) {
             val item = adapter.getItem(index)
