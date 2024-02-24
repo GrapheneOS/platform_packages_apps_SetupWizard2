1572de3b5354a1ba35dcdf7f697db014c842bcd2
Jitesh Singh <jiteshsingh.jts@gmail.com>
2024-02-25 04:32:18 +0530

    adjust ui wordings for secondary user setup mode
    show "user profile" in view texts for secondary user setup mode

--- java/app/grapheneos/setupwizard/view/activity/FinishActivity.kt
+++ java/app/grapheneos/setupwizard/view/activity/FinishActivity.kt
@@ -3,12 +3,13 @@ package app.grapheneos.setupwizard.view.activity
 import android.view.View
 import app.grapheneos.setupwizard.R
 import app.grapheneos.setupwizard.action.FinishActions
+import app.grapheneos.setupwizard.action.SetupWizard.isPrimaryUser
 
 class FinishActivity : SetupWizardActivity(
     R.layout.activity_finish,
     R.drawable.baseline_done_all_glif,
     R.string.you_re_all_set_now,
-    R.string.setup_done_desc
+    if (isPrimaryUser) R.string.device_setup_done_desc else R.string.profile_setup_done_desc
 ) {
     private lateinit var finish: View
 
